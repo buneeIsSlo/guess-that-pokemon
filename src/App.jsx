95f96a5acb293eaf897eba4bf835cbda7e47aa4c
bunee <buneeisslo@gmail.com>
2023-06-07 17:45:10 +0530

    Feat: add immer and guess the pokemon functionality
    Added immer.js and implement a basic guess the pokemon functionality.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,64 @@
 import "@fontsource/press-start-2p";
 import "nes.css/css/nes.min.css";
-import { Button } from "@kylum/nes-react";
 import { useEffect, useState } from "react";
+import { Button } from "@kylum/nes-react";
+import { useImmerReducer } from "use-immer";
 
-const POKE_API_URL = "https://pokeapi.co/api/v2/pokemon?limit=10";
+const POKE_API_URL = "https://pokeapi.co/api/v2/pokemon?limit=1000";
+const initalState = {
+  firstThousand: [],
+  currentQuestion: null,
+};
+
+function reducer(draft, action) {
+  switch (action.type) {
+    case "storeFirstThousand":
+      draft.firstThousand.push(action.value);
+      return;
+    case "currentQuestion":
+      draft.currentQuestion = generateQuestion();
+      console.log(draft.currentQuestion);
+      return;
+    case "guessed":
+      if (draft.currentQuestion.answer === action.value) {
+        console.log("niceooo");
+        draft.currentQuestion = generateQuestion();
+      }
+  }
+
+  function generateQuestion() {
+    const fourRandomPokemons = retrieveRandomFour();
+    console.log(fourRandomPokemons);
+
+    const randomNum = Math.floor(Math.random() * 4);
+    const pokemonToGuess = fourRandomPokemons[randomNum];
+
+    return {
+      pokemonToGuess: pokemonToGuess[1],
+      options: fourRandomPokemons.map((pokemonArr) => pokemonArr[0]),
+      answer: pokemonToGuess[0],
+    };
+  }
+
+  function retrieveRandomFour() {
+    const randomFour = new Set();
+    const numOfOptions = 4;
+
+    while (randomFour.size < numOfOptions) {
+      const randomNum = Math.floor(Math.random() * draft.firstThousand.length);
+      randomFour.add(draft.firstThousand[randomNum]);
+      draft.firstThousand.splice(randomNum, 1);
+    }
+
+    return [...randomFour].map((str) => {
+      const [pokemonName, pokemonSprite] = str.split("@");
+      return [pokemonName, pokemonSprite];
+    });
+  }
+}
 
 function App() {
-  const [test, setTest] = useState(null);
-  const [show, setShow] = useState(false);
+  const [gameState, dispatch] = useImmerReducer(reducer, initalState);
 
   useEffect(() => {
     const reqController = new AbortController();
@@ -18,13 +69,14 @@ function App() {
         const response = await API.json();
         const pokemonURLs = await response.results;
 
-        const tmpArr = [];
         pokemonURLs.map(async (res) => {
           const data = await fetch(`${res.url}`);
-          var { name, sprites } = await data.json();
+          let { name, sprites } = await data.json();
 
-          tmpArr.push(sprites.front_default);
-          setTest(tmpArr);
+          dispatch({
+            type: "storeFirstThousand",
+            value: name + "@" + sprites.front_default,
+          });
         });
       } catch {
         console.log("Error");
@@ -37,20 +89,42 @@ function App() {
     };
   }, []);
 
-  function showPokemons() {}
-
   return (
     <>
       <h1>
         Guess <br></br>that<br></br> Pokemon
       </h1>
-      <Button onClick={() => setShow(true)}>Fetch first 10</Button>
-      {show &&
-        test.map((sprite, i) => {
-          return (
-            <img src={sprite} alt="" key={i} style={{ display: "block" }} />
-          );
-        })}
+      <Button
+        onClick={() => {
+          dispatch({ type: "currentQuestion" });
+          console.log(gameState.firstThousand.length);
+        }}
+      >
+        Generate question
+      </Button>
+      {gameState.currentQuestion && (
+        <div className="question">
+          <div className="pokemon__wrapper">
+            <img
+              src={gameState.currentQuestion.pokemonToGuess}
+              alt=""
+              className="pokemon__sprite"
+            />
+          </div>
+          <div className="options">
+            {gameState.currentQuestion.options.map((opt, i) => {
+              return (
+                <Button
+                  onClick={() => dispatch({ type: "guessed", value: opt })}
+                  key={i}
+                >
+                  {opt}
+                </Button>
+              );
+            })}
+          </div>
+        </div>
+      )}
     </>
   );
 }
