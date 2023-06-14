dc36b02679a3f5b28715b6d098a06ad0798b35f9
bunee <buneeisslo@gmail.com>
2023-06-14 23:39:23 +0530

    Test: effecient api calls
    Trying to reduce the number of api calls.

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ import { useEffect, useState } from "react";
 import { Button } from "@kylum/nes-react";
 import { useImmerReducer } from "use-immer";
 
-const POKE_API_URL = "https://pokeapi.co/api/v2/pokemon?limit=1000";
+const POKE_API_URL = "https://pokeapi.co/api/v2/pokemon?limit=10";
 const MAX_LIVES = 5;
 
 const initalState = {
+  pokemonData: [],
   firstThousand: [],
   currentQuestion: null,
   isPlaying: false,
@@ -32,6 +33,10 @@ function reducer(draft, action) {
         if (draft.lives < 1) draft.isPlaying = false;
         console.log(draft.lives);
       }
+      return;
+    case "getPokemonData":
+      draft.pokemonData.push(action.value);
+      return;
   }
 
   function generateQuestion() {
@@ -68,34 +73,99 @@ function reducer(draft, action) {
 function App() {
   const [gameState, dispatch] = useImmerReducer(reducer, initalState);
 
-  useEffect(() => {
-    const reqController = new AbortController();
+  // useEffect(() => {
+  //   const reqController = new AbortController();
 
-    async function fetchFirstThousand() {
-      try {
-        const API = await fetch(POKE_API_URL, { signal: reqController.signal });
-        const response = await API.json();
-        const pokemonURLs = await response.results;
+  //   async function fetchFirstThousand() {
+  //     try {
+  //       const API = await fetch(POKE_API_URL, { signal: reqController.signal });
+  //       const response = await API.json();
+  //       const pokemonURLs = await response.results;
 
-        pokemonURLs.map(async (res) => {
-          const data = await fetch(`${res.url}`);
-          let { name, sprites } = await data.json();
+  //       pokemonURLs.map(async (res) => {
+  //         const data = await fetch(`${res.url}`);
+  //         let { name, sprites } = await data.json();
 
-          dispatch({
-            type: "storeFirstThousand",
-            value: name + "@" + sprites.front_default,
-          });
-        });
-      } catch {
-        console.log("Error");
-      }
+  //         dispatch({
+  //           type: "storeFirstThousand",
+  //           value: name + "@" + sprites.front_default,
+  //         });
+  //       });
+  //     } catch {
+  //       console.log("Error");
+  //     }
+  //   }
+
+  //   fetchFirstThousand();
+  //   return () => {
+  //     reqController.abort();
+  //   };
+  // }, []);
+
+  function newFetch() {
+    const pokeURLs = JSON.parse(localStorage.getItem("pokeURLs"));
+
+    if (pokeURLs) {
+      dispatch({ type: "getPokemonData", value: pokeURLs });
+      console.log(pokeURLs);
+      pickRandom60();
+      return;
     }
 
-    fetchFirstThousand();
-    return () => {
-      reqController.abort();
-    };
-  }, []);
+    useEffect(() => {
+      const reqController = new AbortController();
+
+      async function getPokemons() {
+        try {
+          const API = await fetch(POKE_API_URL, {
+            signal: reqController.signal,
+          });
+          const response = await API.json();
+          console.log(response);
+          const pokeU = await response.results;
+
+          localStorage.setItem("pokeURLs", JSON.stringify(pokeU));
+          dispatch({ type: "getPokemonData", value: pokeU });
+          pickRandom60();
+        } catch {
+          console.log("Error");
+        }
+      }
+
+      getPokemons();
+      return () => {
+        reqController.abort();
+      };
+    }, []);
+  }
+
+  function pickRandom60() {
+    const random60 = new Set();
+    const maxPokemons = 5;
+
+    while (random60.size <= maxPokemons) {
+      const randomNum = Math.floor(
+        Math.random() * gameState.pokemonData.length
+      );
+      console.log(gameState.pokemonData);
+      random60.add(gameState.pokemonData[randomNum]["name"]);
+      gameState.pokemonData.splice(randomNum, 1);
+    }
+
+    // [...random60].map(async (res) => {
+    //   const data = await fetch(`${res.url}`);
+    //   let { name, sprites } = await data.json();
+
+    //   dispatch({
+    //     type: "storeFirstThousand",
+    //     value: name + "@" + sprites.front_default,
+    //   });
+    // });
+
+    // console.log(random60);
+  }
+
+  newFetch();
 
   return (
     <>
@@ -139,3 +209,41 @@ function App() {
 }
 
 export default App;
+
+/*
+  const endpoint = 'https://pokeapi.co/api/v2/pokemon?limit=1000';
+const numPokemons = 50;
+const pokemons = [];
+
+// Fetch list of all pokemons
+fetch(endpoint)
+  .then(response => response.json())
+  .then(data => {
+    // Randomly select 50 pokemons
+    const randomIndices = new Set();
+    while (randomIndices.size < numPokemons) {
+      randomIndices.add(Math.floor(Math.random() * data.results.length));
+    }
+    const randomPokemons = Array.from(randomIndices).map(i => data.results[i]);
+
+    // Fetch name and sprite for each pokemon
+    return Promise.all(randomPokemons.map(pokemon => {
+      return fetch(pokemon.url)
+        .then(response => response.json())
+        .then(data => {
+          pokemons.push({
+            name: data.name,
+            sprite: data.sprites.front_default
+          });
+        });
+    }));
+  })
+  .then(() => {
+    // Do something with the pokemons array
+    console.log(pokemons);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+  
+ */
