1a0167976133c434d65e715415b32980702539bc
bunee <buneeisslo@gmail.com>
2023-11-17 22:47:00 +0530

    Refactor: reduce re-renders(draft)

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,5 +1,5 @@
 import "./css/main.css";
-import { useEffect } from "react";
+import { useEffect, useCallback, useMemo } from "react";
 import useGameContext from "../hooks/useGameContext";
 import { enableMapSet } from "immer";
 import { POKEMONS_RANGE, MAX_POKEMONS } from "../utils/constants";
@@ -10,23 +10,51 @@ import EndModal from "../components/EndModal";
 
 enableMapSet();
 
+const getRandomNums = (fetchedNums, dispatch) => {
+  const nums = [];
+
+  while (nums.length < MAX_POKEMONS) {
+    const num = Math.floor(Math.random() * POKEMONS_RANGE) + 1;
+    if (!fetchedNums.has(num)) {
+      nums.push(num);
+      dispatch({ type: "addFetched", value: num });
+    }
+  }
+
+  return nums;
+};
+
+const fetchPokemons = async (state, dispatch) => {
+  try {
+    const randomUniqueNums = getRandomNums(state.fetchedNums, dispatch);
+    for (const num of randomUniqueNums) {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${num}`);
+      const data = await response.json();
+      const { name, sprites } = data;
+      dispatch({
+        type: "addPokemon",
+        value: { name, sprite: sprites.front_default },
+      });
+    }
+  } catch (error) {
+    console.error("Error fetching pokemons:", error);
+  } finally {
+    console.log("Finished making requests");
+    if (!state.isPlaying) {
+      dispatch({ type: "doneFetching" });
+    }
+    if (state.isRePlaying) {
+      dispatch({ type: "reStartGame" });
+    }
+  }
+};
+
 const Main = () => {
   const { state, dispatch } = useGameContext();
-
-  const getRandomNums = () => {
-    const { fetchedNums } = state;
-    const nums = [];
-
-    while (nums.length < MAX_POKEMONS) {
-      const num = Math.floor(Math.random() * POKEMONS_RANGE) + 1;
-      if (!fetchedNums.has(num)) {
-        nums.push(num);
-        dispatch({ type: "addFetched", value: num });
-      }
-    }
-
-    return nums;
-  };
+  const fetchPokemonsMemoized = useCallback(
+    () => fetchPokemons(state, dispatch),
+    [state, dispatch]
+  );
 
   useEffect(() => {
     const highscore = localStorage.getItem("highscore");
@@ -41,37 +69,21 @@ const Main = () => {
   }, [state.mainScore]);
 
   useEffect(() => {
-    const fetchPokemons = async () => {
-      try {
-        const randomUniqueNums = getRandomNums();
-        for (const num of randomUniqueNums) {
-          const response = await fetch(
-            `https://pokeapi.co/api/v2/pokemon/${num}`
-          );
-          if (!response.ok) console.log("reason!!");
-          const data = await response.json();
-          const { name, sprites } = data;
-          dispatch({
-            type: "addPokemon",
-            value: { name, sprite: sprites.front_default },
-          });
-        }
-      } catch (error) {
-        a;
-        console.error("Error fetching pokemons:", error);
-      } finally {
-        console.log("Finished making requests");
-        if (!state.isPlaying) {
-          dispatch({ type: "doneFetching" });
-        }
-        if (state.isRePlaying) {
-          dispatch({ type: "reStartGame" });
-        }
-      }
-    };
-
-    fetchPokemons();
+    fetchPokemonsMemoized();
   }, [state.fetchCount]);
+
+  const questionComponent = useMemo(() => {
+    if (state.currentQuestion && state.isPlaying) {
+      return (
+        <Question
+          sprite={state.currentQuestion.sprite}
+          options={state.currentQuestion.options}
+          answer={state.currentQuestion.answer}
+        />
+      );
+    }
+    return null;
+  }, [state.currentQuestion, state.isPlaying]);
 
   return (
     <main id="main">
@@ -79,13 +91,7 @@ const Main = () => {
       {state.doneFetching && !state.currentQuestion && !state.isRePlaying && (
         <StartModal />
       )}
-      {state.currentQuestion && state.isPlaying && (
-        <Question
-          sprite={state.currentQuestion.sprite}
-          options={state.currentQuestion.options}
-          answer={state.currentQuestion.answer}
-        />
-      )}
+      {questionComponent}
       {(state.timeRemaining <= 0 || state.lives <= 0) && !state.isPlaying && (
         <EndModal />
       )}
